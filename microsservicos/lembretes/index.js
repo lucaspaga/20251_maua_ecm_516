823b6cb9d4f34831123c8a5f55f8baefbc01ffda
LUCAS CASTANHO PAGANOTTO CARVALHO <22.00921-3@maua.br>
2025-04-26 12:51:55 -0300

    learn(ap01-mss):lida com o codigo 4.3.13

--- microsservicos/lembretes/index.js
+++ microsservicos/lembretes/index.js
@@ -1,5 +1,7 @@
+const axios = require('axios')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 //API: uma coleção de endpoints
 //um endpoint é caracterizado por
@@ -7,7 +9,70 @@ const app = express()
 //um padrão de acesso
 //uma funcionalidade
 //GET /lembretes () => {}
+//localhost:4000/lembretes
+
+/*
+{
+    1 : {
+        id:1,
+        texto: 'fazer cafe'
+    },
+    2{
+        id:2,
+        texto: 'ir a feira'
+    }
+}
+*/
+
+const baseLembretes = {
+
+}
+
+let id = 1
 
 app.get('/lembretes', (req, res) => {
+    res.json(baseLembretes)
+})
 
+//localhost:4000/lembretes
+//POST /lembretes () => {}
+app.post('/lembretes', async (req, res) => {
+    //const texto = req.body.texto
+    const {texto} = req.body
+
+    const lembrete = {
+        id,
+        texto
+    }
+
+    baseLembretes[id] = lembrete
+    
+    id++
+
+    axios.post('http://localhost:10000/eventos', {
+        tipo: 'LembreteCriado',
+        dados: lembrete
+    })
+    .then((resAxios) => {
+        console.log(resAxios.data)
+    })
+    .catch((e)=>{
+        console.log(e)
+    })
+    .finally(() => {
+        res.status(201).json(lembrete)
+    })
+
+})
+
+//POST /eventos
+app.post('/eventos', (req, res) => {
+    console.log(req.body)
+    res.end()
+})
+
+const port = 4000
+
+app.listen(port, () => {
+    console.log(`Lembretes. Porta:${port}`)
 })
--- microsservicos/observacoes/index.js
+++ microsservicos/observacoes/index.js
@@ -1,1 +1,65 @@
-console.log('oi observacoes')
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+app.use(express.json())
+
+
+/*
+{
+    1:[
+        {
+            id: 1001,
+            idLembrete: 1,
+            texto: Sem açúcar
+        },
+        {
+            id: 1002,
+            idLembrete: 1,
+            texto: Comprar o pó
+        }
+    ],
+    2:[]
+}
+*/
+
+const baseObservacoes = {
+
+}
+
+//GET /lembretes/1/obervacoes
+
+app.get('/lembretes/:idLembrete/observacoes', (req, res) => {
+    const idLembrete = req.params.idLembrete
+
+    if (idLembrete === 'all') res.json(baseObservacoes)
+    else{
+        res.json(baseObservacoes[idLembrete] || [])
+        }   
+})
+
+//POST /lembretes/1/obervacoes
+
+app.post('/lembretes/:idLembrete/observacoes', (req, res) => {
+    const idObservacao = uuidv4()
+    const texto = req.body.texto
+    const{idLembrete} = req.params
+
+    const obervacao = {
+        id: idObservacao,
+        idLembrete,
+        texto
+    }
+    const observacoes = baseObservacoes[idLembrete] || []
+
+    observacoes.push(obervacao)
+
+    baseObservacoes[idLembrete] = observacoes
+
+    res.status(201).json(observacoes)
+
+})
+
+const port = 5000
+app.listen(port, () => {
+    console.log(`Observações. Porta:${port}`)
+})
